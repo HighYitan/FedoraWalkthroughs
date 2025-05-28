a6c66748219e195e7506f64e4da27294c3601e0c
HighYitan <sr.highyitan@gmail.com>
2025-05-28 08:10:35 +0200

    'vistagamesindexcompletar?'

---
+++ resources/views/components/card-games.blade.php
@@ -0,0 +1,53 @@
+@php use Illuminate\Support\Facades\Route; @endphp
+<div class="block rounded-lg bg-white shadow-secondary-1">
+    <div class="p-6 text-surface">
+        <h3 class="mb-2 text-4xl font-medium leading-tight">{{ $game['nombre_inicial'] }}</h3>
+    </div>
+    @if (isset($game['puntuacion_media']))
+        <h5 class="mb-2 text-xl font-medium leading-tight">Puntuación media: {{ $game['puntuacion_media'] }}</h5>
+    @endif
+    <h5 class="mb-2 text-xl font-medium leading-tight">Destacado: <span class="break-all">{{ $game['destacado'] }}</span></h5>
+    @if (isset($game['imagen']))
+        <h5 class="mb-2 text-xl font-medium leading-tight">Imágen: {{ $game['imagen'] }}</h5>
+    @endif
+    @if (isset($game['video']))
+        <h5 class="mb-2 text-xl font-medium leading-tight">Imágen: {{ $game['video'] }}</h5>
+    @endif
+    @if (isset($game['web']))
+        <h5 class="mb-2 text-xl font-medium leading-tight">Imágen: {{ $game['web'] }}</h5>
+    @endif
+    @foreach ($game['traducciones'] as $traduccion)
+        @if ($traduccion['idioma']['abreviatura'] === 'ES')
+            <h5 class="mb-2 text-xl font-medium leading-tight">Descripción ES: {{ $traduccion['descripcion'] }}</h5>
+        @elseif ($traduccion['idioma']['abreviatura'] === 'CA')
+            <h5 class="mb-2 text-xl font-medium leading-tight">Descripción CA: {{ $traduccion['descripcion'] }}</h5>
+        @elseif ($traduccion['idioma']['abreviatura'] === 'EN')
+            <h5 class="mb-2 text-xl font-medium leading-tight">Descripción EN: {{ $traduccion['descripcion'] }}</h5>
+        @endif
+    @endforeach
+    @foreach ($game['generos'] as $genero)
+        @if ($genero['idioma']['abreviatura'] === 'ES')
+            <h5 class="mb-2 text-xl font-medium leading-tight">Género ES: {{ $genero['nombre'] }}</h5>
+        @elseif ($genero['idioma']['abreviatura'] === 'CA')
+            <h5 class="mb-2 text-xl font-medium leading-tight">Género CA: {{ $genero['nombre'] }}</h5>
+        @elseif ($genero['idioma']['abreviatura'] === 'EN')
+            <h5 class="mb-2 text-xl font-medium leading-tight">Género EN: {{ $genero['nombre'] }}</h5>
+        @endif
+    @endforeach
+    <div class="flex gap-2 mt-4">
+        @if (Route::currentRouteName() !== 'game.index')
+            <a href="{{route('game.index')}}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Volver</a>
+        @endif
+        @if (Route::currentRouteName() !== 'game.show')
+            <a href="{{route('game.show', ['game' => $game['id']])}}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Mostrar</a>
+        @endif
+        @if (Route::currentRouteName() !== 'game.edit')
+            <a href="{{route('game.edit', ['game' => $game['id']])}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Editar</a>
+        @endif
+        <form action="{{route('game.destroy', ['game' => $game['id']])}}" method="POST" class="ml-auto">
+            @method('DELETE')
+            @csrf
+            <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Eliminar</button>
+        </form>
+    </div>
+</div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <!-- Link para Videojuegos -->
+                    <x-nav-link :href="route('game.index')" :active="request()->routeIs('game.index')">
+                        {{ __('Videojuegos') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -69,6 +73,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('game.index')" :active="request()->routeIs('game.index')">
+                {{ __('Videojuegos') }}
             </x-responsive-nav-link>
         </div>
 
